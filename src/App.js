1141cbd0d5154be1c22666e8dfe943f604a024e0
Kartik Nama <kartikna0@gmail.com>
2025-01-26 22:04:19 +0530

    Gh-Pages

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import React, {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Pages/Home";
+import Products from "./components/Pages/Products";
+import Admin from "./components/Pages/Admin";
+import SingleProduct from "./components/Pages/Single Product/SingleProduct";
+import Footer from "./components/Footer";
+function App() {
+  let [prods, setProds]  = useState([])
+    
+  useEffect(() => {
+    const fetchProducts = async () =>{
+      const response = await fetch("http://localhost:8080/api/getproducts");
+      const products = await response.json()
+      setProds(products);
+    }
+    fetchProducts()
+
+  })
+
+  const addProductHandler = (newProduct) =>{
+    setProds((prevProds)=>[...prevProds, newProduct]);
+  }
+
+  return (
+    <div className="container-fluid m-0 p-0">
+      <Router>
+        <Header />
+        <div className="app-body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products products={prods} />} />
+            <Route path="/admin" element={<Admin products={prods} onAddProduct={addProductHandler} />} />
+            <Route path="/products/:productId" element={<SingleProduct products={prods}/>} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pages/AddProductForm.js
@@ -0,0 +1,86 @@
+import React, {useState} from "react";
+import './AddProductForm.css';
+import axios from "axios";
+const AddProductForm = (props) => {
+
+  const { onAddProduct} = props
+  const id = Math.floor(Math.random() * 100) + 1;
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredContent, setEnteredContent] = useState('');
+  const [enteredImage, setEnteredImage] = useState(null);
+  const [enteredPrice, setEnteredPrice] = useState('');
+
+  const titleChangeHandler = (e) =>{
+    setEnteredTitle(e.target.value)
+  }
+  const contentChangeHandler = (e) =>{
+    setEnteredContent(e.target.value)
+
+  }
+  const imageChangeHandler = (e) =>{
+    const file = e.target.files[0]
+    setEnteredImage(file);
+  }
+  const priceChangeHandler = (e) =>{
+    setEnteredPrice(e.target.value)
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newProduct = {
+      id: id,
+      title: enteredTitle,
+      content: enteredContent,
+      imageUrl: enteredImage.name,
+      price: enteredPrice 
+    };
+    try {
+      const response = await axios.post("https://backendshop-o7ve.onrender.com/api/products", newProduct)
+
+      if(response.status=== 201){
+        console.log("Product added successfully", response.data)
+        onAddProduct(newProduct);
+      }else{
+        
+        console.error("failed to add")
+      }
+    }
+    catch(error){
+      console.error("Error adding product", error);
+    }
+    
+  };
+
+  return (
+    <div className="container form-container m-auto">
+      <form onSubmit={submitHandler}>
+        <div className="form__ins">
+          <input type="hidden" value={id} />
+          <div className="productadd__element">
+            <label>Title</label>
+            <input type="text" value={enteredTitle} className="product__area" onChange={titleChangeHandler} />
+          </div>
+          <div className="productadd__element">
+            <label>Content</label>
+            <input type="text" value={enteredContent} className="product__area"  onChange={contentChangeHandler} />
+          </div>
+          <div className="productadd__element">
+            <label>Image</label>
+            <input type="file" className="product__area"  onChange={imageChangeHandler} />
+          </div>
+          <div className="productadd__element">
+            <label>Price</label>
+            <input type="number" value={enteredPrice} className="product__area"  onChange={priceChangeHandler} />
+          </div>
+
+        </div>
+          <button className="btn btn-success" type="submit">Add Product</button>
+      <button className="btn btn-danger" onClick={props.onCancel}>
+        Cancel
+      </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductForm;
