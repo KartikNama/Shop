df9576255d88b10a099e9a0df32b7c8cd8158277
Kartik Nama <kartikna0@gmail.com>
2025-01-30 18:54:16 +0530

    Revert "Single Product Image"
    This reverts commit 7fbab93088e35d0e44a2e6a4041041ead6dd4da7.

--- src/components/Pages/Single Product/SingleProduct.js
+++ src/components/Pages/Single Product/SingleProduct.js
@@ -7,7 +7,7 @@ const SingleProduct = ({products}) => {
 
     return(
         <div className="container">
-            <img src={"https://backendshop-o7ve.onrender.com/images/" + product.imageUrl} alt={product.title} />
+            <img src={"../products-images/" + product.imageUrl} alt={product.title} />
             <h1>{product.title}</h1>
             <p>{product.content}</p>
             <p>{product.price}</p>
